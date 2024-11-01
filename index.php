bf5b58521abc4575c67dba19886ccc1312c7f3f6
Esmail514 <asasesmail514@gmail.com>
2024-11-01 14:34:56 -0400

    all files add

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/all.min.css">
+    <link rel="stylesheet" href="CSS/fontawesome.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <form method="post">
+            <h2>Login Now</h2>
+
+            <div class="input-group">
+                <label>UserName</label>
+                <input type="text" name="username" id="username" placeholder="UserName" onkeyup="validateName()">
+                <span id="username-error"></span>
+            </div>
+
+            <div class="input-group">
+                <label>Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" onkeyup="validatePass()">
+                <span id="password-error"></span>
+            </div>
+            <input type="submit" name="submit" class="btn" value="Sign-in" onclick="return validateForm()">
+            <span id="submit-error"></span>
+             <a href="Register.php">Don't you have an account?</a>
+
+</body>
+
+</html>
